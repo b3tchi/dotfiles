71c5d73b233908f3428c24fd649a72fedf42e9aa
Jan Becka <beckaja1@gmail.com>
2020-09-30 06:00:49 +0200

    Ubuntu post install script

---
+++ scripts/ubuntu-postinstall.sh
@@ -0,0 +1,47 @@
+#fish shell
+sudo apt-add-repository ppa:fish-shell/release-3
+sudo apt update
+sudo apt install fish
+chsh -s /usr/bin/fish
+
+#kvantum theme
+sudo add-apt-repository ppa:krisives/kvantum
+sudo apt update
+sudo apt install kvantum
+sudo apt install gtk2-engines-pixbuf gtk2-engines-murrine #gtk themes
+
+#papirus icons
+sudo add-apt-repository ppa:papirus/papirus
+sudo apt update
+sudo apt install papirus-icon-theme
+
+#papirus icon theme
+sudo add-apt-repository ppa:papirus/papirus
+sudo apt update
+sudo apt install qt5-style-kvantum qt5-style-kvantum-themes
+
+#window decorations
+sudo add-apt-repository ppa:krisives/sierrabreezeenhanced
+sudo apt update
+sudo apt install sierrabreezeenhanced
+
+#nodejs
+sudo apt install nodejs
+sudo apt install npm
+
+#python
+sudo apt install python3
+sudo apt install pip3
+
+#neovim
+sudo apt install neovim
+sudo apt install fzf
+sudo apt install ripgrep
+sudo npm install neovim
+pip3 install pynvim
+
+#openscad
+sudo apt install openscad
+pip3 install solidpython
+
+# TBD
