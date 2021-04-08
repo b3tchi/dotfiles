bc8c1d219ab44c15138afdb4783aafadd060f550
Jan <beckaja1@gmail.com>
2021-04-08 02:03:48 +0200

    scripts split core gui

--- scripts/ubuntu/ubuntu-core-postinstall.sh
+++ scripts/ubuntu/ubuntu-core-postinstall.sh
@@ -11,25 +11,25 @@ fish.sh
 # curl -L https://get.oh-my.fish | fish
 
 #kvantum theme
-sudo add-apt-repository ppa:krisives/kvantum
-sudo apt update
-sudo apt install kvantum
-sudo apt install gtk2-engines-pixbuf gtk2-engines-murrine #gtk themes
-
-#papirus icons
-sudo add-apt-repository ppa:papirus/papirus
-sudo apt update
-sudo apt install papirus-icon-theme
-
-#papirus icon theme
-sudo add-apt-repository ppa:papirus/papirus
-sudo apt update
-sudo apt install qt5-style-kvantum qt5-style-kvantum-themes
-
-#window decorations
-sudo add-apt-repository ppa:krisives/sierrabreezeenhanced
-sudo apt update
-sudo apt install sierrabreezeenhanced
+# sudo add-apt-repository ppa:krisives/kvantum
+# sudo apt update
+# sudo apt install kvantum
+# sudo apt install gtk2-engines-pixbuf gtk2-engines-murrine #gtk themes
+#
+# #papirus icons
+# sudo add-apt-repository ppa:papirus/papirus
+# sudo apt update
+# sudo apt install papirus-icon-theme
+#
+# #papirus icon theme
+# sudo add-apt-repository ppa:papirus/papirus
+# sudo apt update
+# sudo apt install qt5-style-kvantum qt5-style-kvantum-themes
+#
+# #window decorations
+# sudo add-apt-repository ppa:krisives/sierrabreezeenhanced
+# sudo apt update
+# sudo apt install sierrabreezeenhanced
 
 #nodejs
 nodejs.sh
@@ -67,11 +67,11 @@ neovim.sh
 # sudo npm install neovim
 # pip3 install pynvim
 
-#brave
-brave.sh
+## brave
+# brave.sh
 
-#openscad
-sudo apt install openscad
-pip3 install solidpython
+# #openscad
+# sudo apt install openscad
+# pip3 install solidpython
 
 # TBD
---
+++ scripts/ubuntu/ubuntu-kde-postinstall.sh
@@ -0,0 +1,27 @@
+#kvantum theme
+sudo add-apt-repository ppa:krisives/kvantum
+sudo apt update
+sudo apt install kvantum
+sudo apt install gtk2-engines-pixbuf gtk2-engines-murrine #gtk themes
+
+#papirus icons
+sudo add-apt-repository ppa:papirus/papirus
+sudo apt update
+sudo apt install papirus-icon-theme
+
+#papirus icon theme
+sudo add-apt-repository ppa:papirus/papirus
+sudo apt update
+sudo apt install qt5-style-kvantum qt5-style-kvantum-themes
+
+#window decorations
+sudo add-apt-repository ppa:krisives/sierrabreezeenhanced
+sudo apt update
+sudo apt install sierrabreezeenhanced
+
+#brave
+brave.sh
+
+#openscad
+sudo apt install openscad
+pip3 install solidpython
