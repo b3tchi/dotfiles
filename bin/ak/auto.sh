a893c6ad6fddb1471a3d56f6a3c16b0a6461c48d
jan-becka <janbecka@lego.com>
2022-06-03 06:18:59 +0200

    keyvault shortcut init

---
+++ bin/ak/auto.sh
@@ -0,0 +1,6 @@
+KEYVAULT_SUBSCRIPTION_0='Pay-As-You-Go'
+KEYVAULT_RG_0='rg-my-ak'
+KEYVAULT_NAME_0='my-ak'
+
+#az cli secret documentation
+#https://docs.microsoft.com/en-us/cli/azure/keyvault/secret?view=azure-cli-latest#az-keyvault-secret-delete
---
+++ bin/ak/main.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+route(){
+
+
+  local file
+  local dirpath
+
+  #handle if its link
+  if [[ -L "$0" ]]; then
+    dirpath=$(readlink -f "$0")
+  fi
+
+  dirpath=$(dirname "$dirpath")/
+
+  #preload
+  if [ -f "${dirpath}auto.sh" ]; then
+    . "${dirpath}auto.sh"
+  fi
+
+  if [ $# -gt 0 ]; then
+
+    while [ $# -gt 0 ]; do
+
+      if [ -d $dirpath$1 ]; then
+
+        dirpath=$dirpath${1}/
+        shift
+
+      elif [ -f $dirpath$1 ]; then
+
+        file=$1
+        shift
+
+      elif [[ $1 == *"--"* ]]; then
+
+        param="${1/--/}"
+        shift
+
+        # if [[ $1 == *"--"* || $# -eq 1 ]]; then
+        if [[ $1 == *"--"* ]]; then
+          declare -g $param=1
+        else
+          declare -g $param="$1"
+          shift
+        fi
+
+      else
+
+        break
+
+      fi
+
+    done
+  fi
+
+  # echo $dirpath$file
+  source $dirpath$file
+
+}
+
+route "$@"
+
