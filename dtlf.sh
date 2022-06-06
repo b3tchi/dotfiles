8def5ffad0672c1c0e064704afe8304c6fe34378
jan-becka <janbecka@lego.com>
2022-06-06 17:15:35 +0200

    updates

--- dtlf.sh
+++ dtlf.sh
@@ -67,15 +67,18 @@ ml $d bin/tat ~/.local/bin/
 ml $d bin/ff ~/.local/bin/
 ml $d bin/nvim-sessionizer ~/.local/bin/
 ml $d bin/nvim-startifier ~/.local/bin/
-ml $d bin/gwt/gwt ~/.local/bin/
+ml $d bin/gwt/main.sh ~/.local/bin/ gwt
+ml $d bin/ak/main.sh ~/.local/bin/ ak
 ml $d bin/azdops/pipeline.sh ~/.local/bin/ azdpipes
 ml $d bin/azdops/variables.sh ~/.local/bin/ azdvars
 ml $d bin/fxs.sh ~/.local/bin/ fxs
+# ml $d bin/gwt/gwt ~/.local/bin/
 
 #bin
-chmod +x $d/bin/*
-chmod +x $d/bin/gwt/gwt*
-chmod +x $d/bin/azdops/*.sh
+chmod +x ${d}bin/*
+chmod +x ${d}bin/gwt/main.sh
+chmod +x ${d}bin/ak/main.sh
+chmod +x ${d}bin/azdops/*.sh
 
 #fish
 ml $d fish/config.fish ~/.config/fish/
