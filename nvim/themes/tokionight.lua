f6bd918b497fed1f023a939928361c776e7730eb
Jan Becka <janbecka@lego.com>
2022-11-25 02:06:44 +0100

    comit items

--- nvim/themes/tokionight.lua
+++ nvim/themes/tokionight.lua
@@ -4,6 +4,26 @@ Plug ('folke/tokyonight.nvim', {branch ='main'})
 
 function Load_theme_tokio()
     vim.cmd[[colorscheme tokyonight-night]]
+    vim.cmd[[set fillchars+=diff:╱]]
+
+    vim.api.nvim_set_hl(0,'Folded',{bg='#222436'})
+    vim.api.nvim_set_hl(0,'ActiveWindow',{bg=''})
+    vim.api.nvim_set_hl(0,'InactiveWindow',{bg='#222436'})
+    vim.api.nvim_set_hl(0,'Normal',{fg='#7aa2f7'})
+    vim.api.nvim_set_hl(0,'DiffDelete',{bg='#37222c', fg='#222436'})
+
+    function Handle_Win_Enter()
+        vim.cmd[[ setlocal winhighlight=Normal:ActiveWindow,NormalNC:InactiveWindow ]]
+    end
+
+    local augr = vim.api.nvim_create_augroup   -- Create/get autocommand group
+    vim.api.nvim_create_autocmd(
+        {'WinEnter'} ,{pattern='*'
+            ,group=augr('WindowManagement',{clear = true})--augr_handle
+            ,callback=Handle_Win_Enter
+        }
+    )
+
 end
 
 local augr = vim.api.nvim_create_augroup   -- Create/get autocommand group
