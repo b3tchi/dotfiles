99764124502dcd146d9f81f26c93639ff2f9c9c5
b3tchi <beckaja1@gmail.com>
2024-12-30 01:04:14 +0100

    adding emacs windows specificts

--- emacs/doom/config.el
+++ emacs/doom/config.el
@@ -32,9 +32,11 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-one
-      doom-font (font-spec :family "Iosevka Nerd Font Mono" :size 16)
-      )
+(setq doom-theme 'doom-one)
+(if (eq system-type 'windows-nt)
+	(setq doom-font (font-spec :family "Iosevka NFM Medium" :size 16)) ;windows
+	(setq doom-font (font-spec :family "Iosevka Nerd Font Mono" :size 16)) ;linux
+  )
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
