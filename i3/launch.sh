13004384a23a4acbd50b118b2f237d08c73eee1b
b3tchi <beckaja1@gmail.com>
2025-02-23 18:17:24 +0100

    polybar fixes

--- i3/launch.sh
+++ i3/launch.sh
@@ -7,10 +7,10 @@ polybar-msg cmd quit
 # killall -q polybar
 
 # Launch bar1 and bar2
-echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar bar1 2>&1 | tee -a /tmp/polybar1.log &
+echo "---" | tee -a /tmp/polybar.log
+polybar 2>&1 | tee -a /tmp/polybar.log &
 disown
-polybar bar2 2>&1 | tee -a /tmp/polybar2.log &
-disown
+# polybar bar2 2>&1 | tee -a /tmp/polybar2.log &
+# disown
 
 echo "Bars launched..."
