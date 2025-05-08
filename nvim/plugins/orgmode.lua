942da807fe6f3c09152e60d9f40feeec6e7bb4eb
b3tchi <beckaja1@gmail.com>
2025-05-08 22:02:05 +0200

    saving

--- nvim/plugins/orgmode.lua
+++ nvim/plugins/orgmode.lua
@@ -1,5 +1,65 @@
 return {
 	"nvim-orgmode/orgmode",
+	dependencies = {
+		"lukas-reineke/headlines.nvim",
+		dependencies = "nvim-treesitter/nvim-treesitter",
+		config = function()
+			require("headlines").setup({
+				org = {
+					query = vim.treesitter.query.parse(
+						"org",
+						[[
+						(headline (stars) @headline)
+						(
+							(expr) @dash
+							(#match? @dash "^-----+$")
+						)
+						(block
+							name: (expr) @_name
+							(#match? @_name "(SRC|src|EXAMPLE|example)")
+						) @codeblock
+						(paragraph . (expr) @quote
+							(#eq? @quote ">")
+						)
+					]]
+					),
+					-- headline_highlights = false,
+					headline_highlights = {
+						"OrgHeadline1",
+						"OrgHeadline2",
+						"OrgHeadline3",
+						"OrgHeadline4",
+						"OrgHeadline5",
+						"OrgHeadline6",
+					},
+					bullet_highlights = {
+						"OrgHeadline1",
+						"OrgHeadline2",
+						"OrgHeadline3",
+						"OrgHeadline4",
+						"OrgHeadline5",
+						"OrgHeadline6",
+					},
+					bullets = { "*", "**", "***", "****" },
+					codeblock_highlight = "CodeBlock",
+					dash_highlight = "Dash",
+					dash_string = "-",
+					quote_highlight = "Quote",
+					quote_string = "┃",
+					fat_headlines = false,
+					-- fat_headline_upper_string = "▄",
+					-- fat_headline_lower_string = "▀",
+				},
+			})
+
+			vim.cmd([[highlight OrgHeadline1 guibg=#211e2d guifg=#bb9af7 gui=bold]])
+			vim.cmd([[highlight OrgHeadline2 guibg=#211e2d guifg=#bb9af7 gui=bold]])
+			vim.cmd([[highlight OrgHeadline3 guibg=#211e2d guifg=#bb9af7 gui=bold]])
+			vim.cmd([[highlight OrgHeadline4 guibg=#211e2d guifg=#bb9af7 gui=bold]])
+			vim.cmd([[highlight OrgHeadline5 guibg=#211e2d guifg=#bb9af7 gui=bold]])
+			vim.cmd([[highlight OrgHeadline6 guibg=#211e2d guifg=#bb9af7 gui=bold]])
+		end,
+	},
 	event = "VeryLazy",
 	ft = { "org" },
 	config = function()
