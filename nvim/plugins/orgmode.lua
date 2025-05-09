b4022314cfcd5b774fba65221675e729aeb5778e
b3tchi <beckaja1@gmail.com>
2025-05-09 11:09:37 +0200

    theming

--- nvim/plugins/orgmode.lua
+++ nvim/plugins/orgmode.lua
@@ -16,8 +16,13 @@ return {
 						)
 						(block
 							name: (expr) @_name
+							contents: (contents) @codeblock
 							(#match? @_name "(SRC|src|EXAMPLE|example)")
-						) @codeblock
+						)
+						;inline blocks
+						;((expr) @quote
+						;	(#match? @quote "[~].+[~]")
+						;)
 						(paragraph . (expr) @quote
 							(#eq? @quote ">")
 						)
@@ -33,18 +38,19 @@ return {
 						"OrgHeadline6",
 					},
 					bullet_highlights = {
-						"OrgHeadline1",
-						"OrgHeadline2",
-						"OrgHeadline3",
-						"OrgHeadline4",
-						"OrgHeadline5",
-						"OrgHeadline6",
+						"OrgHeadline1Bullet",
+						"OrgHeadline2Bullet",
+						"OrgHeadline3Bullet",
+						"OrgHeadline4Bullet",
+						"OrgHeadline5Bullet",
+						"OrgHeadline6Bullet",
 					},
-					bullets = { "*", "**", "***", "****" },
+					bullets = { "✦", "✦✦", "✦✦✦", "✦✦✦✦", "✦✦✦✦✦", "✦✦✦✦✦✦" },
 					codeblock_highlight = "CodeBlock",
 					dash_highlight = "Dash",
 					dash_string = "-",
 					quote_highlight = "Quote",
+					-- inlineblock_highlight = "OrgInlineBlock",
 					quote_string = "┃",
 					fat_headlines = false,
 					-- fat_headline_upper_string = "▄",
@@ -52,12 +58,20 @@ return {
 				},
 			})
 
-			vim.cmd([[highlight OrgHeadline1 guibg=#211e2d guifg=#bb9af7 gui=bold]])
-			vim.cmd([[highlight OrgHeadline2 guibg=#211e2d guifg=#bb9af7 gui=bold]])
-			vim.cmd([[highlight OrgHeadline3 guibg=#211e2d guifg=#bb9af7 gui=bold]])
-			vim.cmd([[highlight OrgHeadline4 guibg=#211e2d guifg=#bb9af7 gui=bold]])
-			vim.cmd([[highlight OrgHeadline5 guibg=#211e2d guifg=#bb9af7 gui=bold]])
-			vim.cmd([[highlight OrgHeadline6 guibg=#211e2d guifg=#bb9af7 gui=bold]])
+			-- vim.cmd([[highlight OrgHeadline1 guibg=#211e2d guifg=#bb9af7 gui=bold]])
+			vim.cmd([[highlight OrgHeadline1 guifg=#bb9af7 gui=bold]])
+			vim.cmd([[highlight OrgHeadline2 guifg=#bb9af7 gui=bold]])
+			vim.cmd([[highlight OrgHeadline3 guifg=#bb9af7 gui=bold]])
+			vim.cmd([[highlight OrgHeadline4 guifg=#bb9af7 gui=bold]])
+			vim.cmd([[highlight OrgHeadline5 guifg=#bb9af7 gui=bold]])
+			vim.cmd([[highlight OrgHeadline6 guifg=#bb9af7 gui=bold]])
+
+			vim.cmd([[highlight OrgHeadline1Bullet guifg=#ff7577 gui=bold]])
+			vim.cmd([[highlight OrgHeadline2Bullet guifg=#ff7577 gui=bold]])
+			vim.cmd([[highlight OrgHeadline3Bullet guifg=#ff7577 gui=bold]])
+			vim.cmd([[highlight OrgHeadline4Bullet guifg=#ff7577 gui=bold]])
+			vim.cmd([[highlight OrgHeadline5Bullet guifg=#ff7577 gui=bold]])
+			vim.cmd([[highlight OrgHeadline6Bullet guifg=#ff7577 gui=bold]])
 		end,
 	},
 	event = "VeryLazy",
