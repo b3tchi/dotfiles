514552f300aba29328f6ed9dcb9562f2806fc750
b3tchi <beckaja1@gmail.com>
2025-05-18 21:05:21 +0200

    minor fixes

---
+++ wezterm/wezterm-back.lua
@@ -0,0 +1,110 @@
+local wezterm = require("wezterm")
+local mux = wezterm.mux
+
+local wsl_domains
+local default_domain
+local font_dirs
+local font_locator
+local font
+
+if wezterm.target_triple == "x86_64-pc-windows-msvc" then
+	wsl_domains = wezterm.default_wsl_domains()
+
+	--    wsl_domains = {
+	--        {
+	--            name = 'wsl',
+	--            distribution = 'Ubuntu-20.04',
+	--            default_cwd = "~",
+	--        }
+	--    }
+
+	for idx, dom in ipairs(wsl_domains) do
+		if dom.name == "WSL:Ubuntu-20.04" then
+			dom.default_prog = { "nu" }
+		end
+	end
+
+	--    default_domain = "WSL:Ubuntu-20.04"
+	default_domain = "local"
+
+	--FONTS
+	-- font_dirs = { "fonts" }
+	-- else
+	-- font_dirs = {}
+	-- font_locator = ""
+end
+
+-- font_dirs = {}
+-- font_locator = ""
+font = wezterm.font("Iosevka Nerd Font Mono")
+-- wezterm.on('gui-startup', function(cmd)
+
+--     local args = {}
+--     if cmd then
+--       args = cmd.args
+--     end
+
+--     -- Set a workspace for coding on a current project
+--     -- Top pane is for the editor, bottom pane is for the build tool
+--     -- local project_dir = wezterm.home_dir .. '/wezterm'
+--     local tab, build_pane, window = mux.spawn_window {
+--       workspace = 'default',
+--     --   cwd = project_dir,
+--       args = args,
+--     }
+
+-- --   local tab, pane, window = mux.spawn_window(cmd or {})
+--   -- Create a split occupying the right 1/3 of the screen
+--   --pane:split { size = 0.3 }
+--   -- Create another split in the right of the remaining 2/3
+--   -- of the space; the resultant split is in the middle
+--   -- 1/3 of the display and has the focus.
+--   print('ongui')
+--   --pane:split { size = 0.5 }
+-- end)
+
+return {
+	--WSL
+	-- default_domain = default_domain,
+	-- wsl_domains = wsl_domains,
+
+	--WINDOW CONFIG
+	enable_tab_bar = false,
+	enable_scroll_bar = false,
+	window_padding = {
+		left = 0,
+		right = 0,
+		top = 0,
+		bottom = 0,
+	},
+
+	--FONT
+	-- font_dirs = font_dirs,
+	-- font_locator = font_locator,
+	font = font,
+
+	--COLORSCHEME
+	-- color_scheme = "Gruvbox Dark",
+	color_scheme = "tokyonight_night",
+
+	--MUX Testing
+	-- exit_behavior= "Hold",
+	-- default_prog = { 'nu' }
+	default_prog = { "nu" },
+	--KEYS
+	--[[keys = {
+        {
+            key = 'c',
+            mods = 'CTRL',
+            action = wezterm.action_callback(function(window, pane)
+                if pane:is_alt_screen_active() then
+                    window:perform_action(wezterm.action.SendKey{ key='c', mods='CTRL' }, pane)
+                else
+                    window:perform_action(wezterm.action{ CopyTo = 'ClipboardAndPrimarySelection' }, pane)
+                end
+            end),
+        },
+        { key = 'v', mods = 'CTRL', action = wezterm.action{ PasteFrom = 'Clipboard' } },
+    },
+    ]]
+}
--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,110 +1,391 @@
+-- Pull in the wezterm API
+local os = require("os")
 local wezterm = require("wezterm")
+local session_manager = require("wezterm-session-manager/session-manager")
+local act = wezterm.action
 local mux = wezterm.mux
 
-local wsl_domains
-local default_domain
-local font_dirs
-local font_locator
-local font
+-- --------------------------------------------------------------------
+-- FUNCTIONS AND EVENT BINDINGS
+-- --------------------------------------------------------------------
 
-if wezterm.target_triple == "x86_64-pc-windows-msvc" then
-	wsl_domains = wezterm.default_wsl_domains()
+-- Session Manager event bindings
+-- See https://github.com/danielcopper/wezterm-session-manager
+wezterm.on("save_session", function(window)
+	session_manager.save_state(window)
+end)
+wezterm.on("load_session", function(window)
+	session_manager.load_state(window)
+end)
+wezterm.on("restore_session", function(window)
+	session_manager.restore_state(window)
+end)
 
-	--    wsl_domains = {
-	--        {
-	--            name = 'wsl',
-	--            distribution = 'Ubuntu-20.04',
-	--            default_cwd = "~",
-	--        }
-	--    }
+-- Wezterm <-> nvim pane navigation
+-- You will need to install https://github.com/aca/wezterm.nvim
+-- and ensure you export NVIM_LISTEN_ADDRESS per the README in that repo
 
-	for idx, dom in ipairs(wsl_domains) do
-		if dom.name == "WSL:Ubuntu-20.04" then
-			dom.default_prog = { "nu" }
-		end
+local move_around = function(window, pane, direction_wez, direction_nvim)
+	local result = os.execute(
+		"env NVIM_LISTEN_ADDRESS=/tmp/nvim"
+			.. pane:pane_id()
+			.. " "
+			.. wezterm.home_dir
+			.. "/.local/bin/wezterm.nvim.navigator"
+			.. " "
+			.. direction_nvim
+	)
+	if result then
+		window:perform_action(act({ SendString = "\x17" .. direction_nvim }), pane)
+	else
+		window:perform_action(act({ ActivatePaneDirection = direction_wez }), pane)
 	end
-
-	--    default_domain = "WSL:Ubuntu-20.04"
-	default_domain = "local"
-
-	--FONTS
-	-- font_dirs = { "fonts" }
-	-- else
-	-- font_dirs = {}
-	-- font_locator = ""
 end
 
--- font_dirs = {}
--- font_locator = ""
-font = wezterm.font("Iosevka Nerd Font Mono")
--- wezterm.on('gui-startup', function(cmd)
+wezterm.on("move-left", function(window, pane)
+	move_around(window, pane, "Left", "h")
+end)
 
---     local args = {}
---     if cmd then
---       args = cmd.args
---     end
+wezterm.on("move-right", function(window, pane)
+	move_around(window, pane, "Right", "l")
+end)
 
---     -- Set a workspace for coding on a current project
---     -- Top pane is for the editor, bottom pane is for the build tool
---     -- local project_dir = wezterm.home_dir .. '/wezterm'
---     local tab, build_pane, window = mux.spawn_window {
---       workspace = 'default',
---     --   cwd = project_dir,
---       args = args,
---     }
+wezterm.on("move-up", function(window, pane)
+	move_around(window, pane, "Up", "k")
+end)
 
--- --   local tab, pane, window = mux.spawn_window(cmd or {})
---   -- Create a split occupying the right 1/3 of the screen
---   --pane:split { size = 0.3 }
---   -- Create another split in the right of the remaining 2/3
---   -- of the space; the resultant split is in the middle
---   -- 1/3 of the display and has the focus.
---   print('ongui')
---   --pane:split { size = 0.5 }
--- end)
+wezterm.on("move-down", function(window, pane)
+	move_around(window, pane, "Down", "j")
+end)
 
-return {
-	--WSL
-	-- default_domain = default_domain,
-	-- wsl_domains = wsl_domains,
+local vim_resize = function(window, pane, direction_wez, direction_nvim)
+	local result = os.execute(
+		"env NVIM_LISTEN_ADDRESS=/tmp/nvim"
+			.. pane:pane_id()
+			.. " "
+			.. wezterm.home_dir
+			.. "/.local/bin/wezterm.nvim.navigator"
+			.. " "
+			.. direction_nvim
+	)
+	if result then
+		window:perform_action(act({ SendString = "\x1b" .. direction_nvim }), pane)
+	else
+		window:perform_action(act({ ActivatePaneDirection = direction_wez }), pane)
+	end
+end
 
-	--WINDOW CONFIG
-	enable_tab_bar = false,
-	enable_scroll_bar = false,
-	window_padding = {
-		left = 0,
-		right = 0,
-		top = 0,
-		bottom = 0,
+wezterm.on("resize-left", function(window, pane)
+	vim_resize(window, pane, "Left", "h")
+end)
+
+wezterm.on("resize-right", function(window, pane)
+	vim_resize(window, pane, "Right", "l")
+end)
+
+wezterm.on("resize-up", function(window, pane)
+	vim_resize(window, pane, "Up", "k")
+end)
+
+wezterm.on("resize-down", function(window, pane)
+	vim_resize(window, pane, "Down", "j")
+end)
+
+-- --------------------------------------------------------------------
+-- CONFIGURATION
+-- --------------------------------------------------------------------
+
+-- This table will hold the configuration.
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+	config = wezterm.config_builder()
+end
+
+config.adjust_window_size_when_changing_font_size = false
+config.automatically_reload_config = true
+config.color_scheme = "tokyonight_night"
+config.enable_scroll_bar = false
+config.enable_wayland = true
+-- config.font = wezterm.font('Hack')
+config.font = wezterm.font("Iosevka Nerd Font Mono")
+config.font_size = 12.0
+config.hide_tab_bar_if_only_one_tab = true
+config.default_prog = { "nu" }
+-- The leader is similar to how tmux defines a set of keys to hit in order to
+-- invoke tmux bindings. Binding to ctrl-a here to mimic tmux
+config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 2000 }
+config.mouse_bindings = {
+	-- Open URLs with Ctrl+Click
+	{
+		event = { Up = { streak = 1, button = "Left" } },
+		mods = "CTRL",
+		action = act.OpenLinkAtMouseCursor,
+	},
+}
+config.pane_focus_follows_mouse = true
+config.scrollback_lines = 5000
+config.use_dead_keys = false
+config.warn_about_missing_glyphs = false
+config.window_decorations = "TITLE | RESIZE"
+config.window_padding = {
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
+}
+
+-- Tab bar
+config.use_fancy_tab_bar = true
+config.tab_bar_at_bottom = true
+-- config.switch_to_last_active_tab_when_closing_tab = true
+config.switch_to_last_active_tab_when_closing_tab = false
+config.tab_max_width = 32
+config.colors = {
+	tab_bar = {
+		active_tab = {
+			fg_color = "#073642",
+			bg_color = "#2aa198",
+		},
+	},
+}
+
+-- Setup muxing by default
+config.unix_domains = {
+	{
+		name = "unix",
+	},
+}
+
+-- Custom key bindings
+config.keys = {
+	-- -- Disable Alt-Enter combination (already used in tmux to split pane)
+	-- {
+	--     key = 'Enter',
+	--     mods = 'ALT',
+	--     action = act.DisableDefaultAssignment,
+	-- },
+
+	-- Copy mode
+	{
+		key = "[",
+		mods = "LEADER",
+		action = act.ActivateCopyMode,
 	},
 
-	--FONT
-	-- font_dirs = font_dirs,
-	-- font_locator = font_locator,
-	font = font,
+	-- ----------------------------------------------------------------
+	-- TABS
+	--
+	-- Where possible, I'm using the same combinations as I would in tmux
+	-- ----------------------------------------------------------------
 
-	--COLORSCHEME
-	-- color_scheme = "Gruvbox Dark",
-	color_scheme = "tokyonight_night",
+	-- Show tab navigator; similar to listing panes in tmux
+	{
+		key = "w",
+		mods = "LEADER",
+		action = act.ShowTabNavigator,
+	},
+	-- Create a tab (alternative to Ctrl-Shift-Tab)
+	{
+		key = "c",
+		mods = "LEADER",
+		action = act.SpawnTab("CurrentPaneDomain"),
+	},
+	-- Rename current tab; analagous to command in tmux
+	{
+		key = ",",
+		mods = "LEADER",
+		action = act.PromptInputLine({
+			description = "Enter new name for tab",
+			action = wezterm.action_callback(function(window, pane, line)
+				if line then
+					window:active_tab():set_title(line)
+				end
+			end),
+		}),
+	},
+	-- Move to next/previous TAB
+	{
+		key = "n",
+		mods = "LEADER",
+		action = act.ActivateTabRelative(1),
+	},
+	{
+		key = "p",
+		mods = "LEADER",
+		action = act.ActivateTabRelative(-1),
+	},
+	-- Close tab
+	{
+		key = "&",
+		mods = "LEADER|SHIFT",
+		action = act.CloseCurrentTab({ confirm = true }),
+	},
 
-	--MUX Testing
-	-- exit_behavior= "Hold",
-	-- default_prog = { 'nu' }
-	default_prog = { "nu" },
-	--KEYS
-	--[[keys = {
-        {
-            key = 'c',
-            mods = 'CTRL',
-            action = wezterm.action_callback(function(window, pane)
-                if pane:is_alt_screen_active() then
-                    window:perform_action(wezterm.action.SendKey{ key='c', mods='CTRL' }, pane)
-                else
-                    window:perform_action(wezterm.action{ CopyTo = 'ClipboardAndPrimarySelection' }, pane)
-                end
-            end),
-        },
-        { key = 'v', mods = 'CTRL', action = wezterm.action{ PasteFrom = 'Clipboard' } },
-    },
-    ]]
+	-- ----------------------------------------------------------------
+	-- PANES
+	--
+	-- These are great and get me most of the way to replacing tmux
+	-- entirely, particularly as you can use "wezterm ssh" to ssh to another
+	-- server, and still retain Wezterm as your terminal there.
+	-- ----------------------------------------------------------------
+
+	-- -- Vertical split
+	{
+		-- |
+		key = "|",
+		mods = "LEADER|SHIFT",
+		action = act.SplitPane({
+			direction = "Right",
+			size = { Percent = 50 },
+		}),
+	},
+	-- Horizontal split
+	{
+		-- -
+		key = "-",
+		mods = "LEADER",
+		action = act.SplitPane({
+			direction = "Down",
+			size = { Percent = 50 },
+		}),
+	},
+	-- CTRL + (h,j,k,l) to move between panes
+	{
+		key = "h",
+		mods = "CTRL",
+		action = act({ EmitEvent = "move-left" }),
+	},
+	{
+		key = "j",
+		mods = "CTRL",
+		action = act({ EmitEvent = "move-down" }),
+	},
+	{
+		key = "k",
+		mods = "CTRL",
+		action = act({ EmitEvent = "move-up" }),
+	},
+	{
+		key = "l",
+		mods = "CTRL",
+		action = act({ EmitEvent = "move-right" }),
+	},
+	-- ALT + (h,j,k,l) to resize panes
+	{
+		key = "h",
+		mods = "ALT",
+		action = act({ EmitEvent = "resize-left" }),
+	},
+	{
+		key = "j",
+		mods = "ALT",
+		action = act({ EmitEvent = "resize-down" }),
+	},
+	{
+		key = "k",
+		mods = "ALT",
+		action = act({ EmitEvent = "resize-up" }),
+	},
+	{
+		key = "l",
+		mods = "ALT",
+		action = act({ EmitEvent = "resize-right" }),
+	},
+	-- Close/kill active pane
+	{
+		key = "x",
+		mods = "LEADER",
+		action = act.CloseCurrentPane({ confirm = true }),
+	},
+	-- Swap active pane with another one
+	{
+		key = "{",
+		mods = "LEADER|SHIFT",
+		action = act.PaneSelect({ mode = "SwapWithActiveKeepFocus" }),
+	},
+	-- Zoom current pane (toggle)
+	{
+		key = "z",
+		mods = "LEADER",
+		action = act.TogglePaneZoomState,
+	},
+	{
+		key = "f",
+		mods = "ALT",
+		action = act.TogglePaneZoomState,
+	},
+	-- Move to next/previous pane
+	{
+		key = ";",
+		mods = "LEADER",
+		action = act.ActivatePaneDirection("Prev"),
+	},
+	{
+		key = "o",
+		mods = "LEADER",
+		action = act.ActivatePaneDirection("Next"),
+	},
+
+	-- ----------------------------------------------------------------
+	-- Workspaces
+	--
+	-- These are roughly equivalent to tmux sessions.
+	-- ----------------------------------------------------------------
+
+	-- Attach to muxer
+	{
+		key = "a",
+		mods = "LEADER",
+		action = act.AttachDomain("unix"),
+	},
+
+	-- Detach from muxer
+	{
+		key = "d",
+		mods = "LEADER",
+		action = act.DetachDomain({ DomainName = "unix" }),
+	},
+
+	-- Show list of workspaces
+	{
+		key = "s",
+		mods = "LEADER",
+		action = act.ShowLauncherArgs({ flags = "WORKSPACES" }),
+	},
+	-- Rename current session; analagous to command in tmux
+	{
+		key = "$",
+		mods = "LEADER|SHIFT",
+		action = act.PromptInputLine({
+			description = "Enter new name for session",
+			action = wezterm.action_callback(function(window, pane, line)
+				if line then
+					mux.rename_workspace(window:mux_window():get_workspace(), line)
+				end
+			end),
+		}),
+	},
+
+	-- Session manager bindings
+	{
+		key = "s",
+		mods = "LEADER|SHIFT",
+		action = act({ EmitEvent = "save_session" }),
+	},
+	{
+		key = "L",
+		mods = "LEADER|SHIFT",
+		action = act({ EmitEvent = "load_session" }),
+	},
+	{
+		key = "R",
+		mods = "LEADER|SHIFT",
+		action = act({ EmitEvent = "restore_session" }),
+	},
 }
+
+-- and finally, return the configuration to wezterm
+return config
