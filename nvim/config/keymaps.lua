9b819fd24dccd716b74f03f777f15ff5d5e6d15c
b3tchi <beckaja1@gmail.com>
2025-05-30 15:57:11 +0200

    sanitizing render md

--- nvim/config/keymaps.lua
+++ nvim/config/keymaps.lua
@@ -9,21 +9,22 @@
 vim.keymap.set("i", "jj", "<ESC>", { noremap = true, silent = true, desc = "<ESC>" })
 
 vim.keymap.set(
-  "n",
-  "<leader>rc",
-  ":%s/<C-r><C-w>//gc<Left><Left><Left>",
-  { noremap = true, silent = true, desc = "replace word under cursor" }
+	"n",
+	"<leader>rc",
+	":%s/<C-r><C-w>//gc<Left><Left><Left>",
+	{ noremap = true, silent = true, desc = "replace word under cursor" }
 )
 vim.keymap.set(
-  "n",
-  "<leader>rr",
-  ':%s/<C-r>"//gc<Left><Left><Left>',
-  { noremap = true, silent = true, desc = "replace word from clipboard" }
+	"n",
+	"<leader>rr",
+	':%s/<C-r>"//gc<Left><Left><Left>',
+	{ noremap = true, silent = true, desc = "replace word from clipboard" }
 )
 
-
 --keep clipboard same when pasting over content
-vim.keymap.set("x", "p", function() return 'pgv"' .. vim.v.register .. "y" end, { remap = false, expr = true })
+vim.keymap.set("x", "p", function()
+	return 'pgv"' .. vim.v.register .. "y"
+end, { remap = false, expr = true })
 -- vim.keymap.set("n", "<leader>dH", "<leader>dh", { noremap = true, silent = true, desc = "triggerDh" })
 -- nnoremap <space>rc :%s/<C-r><C-w>//gc<Left><Left><Left>
 -- nnoremap <space>rr :%s/<C-r>"//gc<Left><Left><Left>
@@ -32,3 +33,9 @@ vim.keymap.set("n", "<leader>ws", "<C-w>v", { noremap = true, silent = true, des
 vim.keymap.set("n", "<leader>wb", "<C-w>s", { noremap = true, silent = true, desc = "split window bellow" })
 -- nnoremap <space>ws <c-w>v
 -- nnoremap <space>wb <c-w>s
+vim.cmd([[highlight @markup.heading.1.markdown guifg=#bb9af7 gui=bold]])
+vim.cmd([[highlight @markup.heading.2.markdown guifg=#bb9af7 gui=bold]])
+vim.cmd([[highlight @markup.heading.3.markdown guifg=#bb9af7 gui=bold]])
+vim.cmd([[highlight @markup.heading.4.markdown guifg=#bb9af7 gui=bold]])
+vim.cmd([[highlight @markup.heading.5.markdown guifg=#bb9af7 gui=bold]])
+vim.cmd([[highlight @markup.heading.6.markdown guifg=#bb9af7 gui=bold]])
--- nvim/plugins/lang-md.lua
+++ nvim/plugins/lang-md.lua
@@ -1,15 +1,36 @@
 return {
-	{
-		"MeanderingProgrammer/render-markdown.nvim",
-		opts = {
-			heading = {
-				width = "block",
-				backgrouds = {},
+	"MeanderingProgrammer/render-markdown.nvim",
+	opts = {
+		heading = {
+			enabled = true,
+			atx = true, --icons
+			setext = false, --icons
+			width = "block",
+			icons = { "✦ ", "✦✦ ", "✦✦✦ ", "✦✦✦✦ ", "✦✦✦✦✦ ", "✦✦✦✦✦✦ " },
+			backgrounds = {
+				false,
+				false,
+				false,
+				false,
+				false,
+				false,
 			},
-			code = {
-				sign = false,
-				width = "full",
+			foregrounds = {
+				"DiagnosticError",
+				"DiagnosticError",
+				"DiagnosticError",
+				"DiagnosticError",
+				"DiagnosticError",
+				"DiagnosticError",
 			},
+		},
+		code = {
+			sign = false,
+			width = "block",
+			border = "thick",
+			min_width = 80,
+			position = "right",
+			language_icon = false,
 		},
 	},
 }
