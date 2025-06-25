3a7f71bb61984e3e6ac8a086ad4f83925ad51d7b
b3tchi <beckaja1@gmail.com>
2025-06-25 14:43:55 +0200

    ufo disable unfold on close

--- nvim/plugins/nvim-ufo.lua
+++ nvim/plugins/nvim-ufo.lua
@@ -6,14 +6,14 @@ return {
 		-- vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
 		vim.o.foldcolumn = "1" -- '0' is not bad
 		vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
-		vim.o.foldlevelstart = 99
+		vim.o.foldlevelstart = -1
 		vim.o.foldenable = true
 	end,
-	config = function()
-		require("ufo").setup({
-			provider_selector = function(bufnr, filetype, buftype)
-				return { "treesitter", "indent" }
-			end,
-		})
-	end,
+	-- config = function()
+	-- 	require("ufo").setup({
+	-- 		provider_selector = function(bufnr, filetype, buftype)
+	-- 			return { "treesitter", "indent" }
+	-- 		end,
+	-- 	})
+	-- end,
 }
